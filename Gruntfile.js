a60dcb7ae07b1efaf3b5f2c17cc69bf078f5c9cf
Mirco <mirco.zeiss@gmail.com>
2013-10-29 18:38:47 +0100

    use cssmin for minification

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,26 +3,29 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     stylus: {
-      dev: {
+      compile: {
         options: {
           compress: false
         },
         files: {
           'flexbox-grid.css': 'flexbox-grid.styl'
         }
-      },
-      prod: {
+      }
+    },
+    cssmin: {
+      minify: {
         options: {
-          compress: true
+          report: 'gzip'
         },
         files: {
-          'flexbox-grid.min.css': 'flexbox-grid.styl'
+          'flexbox-grid.min.css': 'flexbox-grid.css'
         }
       }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-stylus');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
 
-  grunt.registerTask('default', ['stylus']);
+  grunt.registerTask('default', ['stylus', 'cssmin']);
 };
